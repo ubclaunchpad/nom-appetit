8a4c22b941e2ac41e6076a4e883d232a7d416ec4
avivamei <avivamei@students.cs.ubc.ca>
2023-10-14 12:51:08 -0700

    add components and styles folders

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,6 @@
 // home page
 import React, { useEffect, useState } from "react";
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen({navigation}) {
     
-    return (
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-        </View>  
-    )
 }
