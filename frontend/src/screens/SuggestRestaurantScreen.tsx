8740ed735148518f238ba9d9309b99f1bd830b97
avivamei <avivamei@students.cs.ubc.ca>
2023-10-21 11:44:42 -0700

    Add buttons

--- frontend/src/screens/SuggestRestaurantScreen.tsx
+++ frontend/src/screens/SuggestRestaurantScreen.tsx
@@ -1,10 +1,14 @@
 // suggest a restaurant page
 import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import { Button, View } from "react-native";
 
 export default function SuggestRestaurantScreen({navigation}) {
     return (
         <View>
+            <Button 
+            title="Go Back"
+            onPress={ () => navigation.navigate('Home')}
+            />
         </View>
     )
 }
