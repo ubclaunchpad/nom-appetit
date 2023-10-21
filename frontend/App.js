b7274812ed2826fb11917cff4b7b33a62a7012dd
Jay Park <jaybinpark@gmail.com>
2023-10-21 11:38:47 -0700

    added google map routes for native

--- frontend/App.js
+++ frontend/App.js
@@ -2,11 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-// FIX THE CODE 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>goteu</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
