685fb986c6f86adb4aa17dc731b7fc88ef456cec
Jay Park <jaybinpark@gmail.com>
2023-10-21 11:36:14 -0700

    map-route tool added

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// FIX THE CODE 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>goteu</Text>
       <StatusBar style="auto" />
     </View>
   );
