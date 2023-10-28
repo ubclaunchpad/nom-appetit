f41175d118229d2b1392fc841b048692e75b5023
Boris Wang <boris.wang117@gmail.com>
2023-10-28 13:59:56 -0600

    Add core elements to home screen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,28 +1,57 @@
 // home page
 import React, { useEffect, useState } from "react";
-import { Button, View } from "react-native";
+import { Button, View, StyleSheet, Text, TextInput } from "react-native";
 
 export default function HomeScreen({navigation}) {
     return (
-        <View>
+        <View style={style.container}>
+            <Text
+            style={{fontSize: 40, textAlign: 'center'}}>
+                What would you like to eat?
+            </Text>
+
+            <TextInput
+                style={style.input}
+                placeholder="Search for a restaurant"
+            />
+
             <Button 
-            title="View Restaurant"
+            title="View Saved Places"
             onPress={ () => navigation.navigate('View')}
             />
             <Button 
-            title="Suggest Restaurant"
+            title="Suggest a New Place"
             onPress={ () => navigation.navigate('Suggest')}
-            />
-
-            <Button 
-            title="Go Back"
-            onPress={ () => navigation.navigate('Signup')}
-            />
-
-            <Button 
-            title="Search"
-            onPress={ () => navigation.navigate('Search')}
             />
         </View>
     )
 }
+
+const style = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#DCE7C2',
+    },
+
+    text_header1 : {
+        color: '#FFFCF1',
+        fontSize: 16,
+    },
+
+    text_header2 : {
+        color: '#FFFCF1',
+        fontSize: 32,
+    },
+    input: {
+        backgroundColor: 'white',
+        borderColor: '#e8e8e8',
+        borderWidth: 1,
+        borderRadius: 5,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        marginVertical: 5
+    }
+});
