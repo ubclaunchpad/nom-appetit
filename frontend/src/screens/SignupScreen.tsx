edb4894b0dad818599d7c3d7eb9b0a347c3340b4
Jason Lee <jason0409lee@gmail.com>
2023-10-28 12:10:03 -0700

    Random signup stuff

--- frontend/src/screens/SignupScreen.tsx
+++ frontend/src/screens/SignupScreen.tsx
@@ -1,14 +1,45 @@
 // signup page
 import React, { useEffect, useState } from "react";
-import { Button, View } from "react-native";
+import { Button, View, StyleSheet, Text, TouchableOpacity } from "react-native";
+
+const style = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#DCE7C2',
+    },
+
+    text_header1 : {
+        color: '#FFFCF1',
+        fontSize: 16,
+    },
+
+    text_header2 : {
+        color: '#FFFCF1',
+        fontSize: 32,
+    }
+});
 
 export default function SignupScreen({navigation}) {
     return (
-        <View>
+        
+        <View style={style.container}>
+
+            <Text style={style.text_header1}>
+                <Text style={style.text_header1}>Welcome to</Text>
+            </Text>
+
+            <Text style={style.text_header2}>
+                <Text style={style.text_header2}>Nom Appetit</Text>
+            </Text>
+
             <Button 
             title="Test Sign In"
             onPress={ () => navigation.navigate('Home')}
             />
         </View>
+        
     )
 }
