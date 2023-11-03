1e76c067921dd6e3580e1a647923a1ccb332f80c
zhanginc <zhanginc@students.cs.ubc.ca>
2023-11-03 00:58:25 -0700

    adds restaurant card component

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -22,20 +22,3 @@ const styles = StyleSheet.create({
 
 })
 
-// const styles = StyleSheet.create({
-//   button: {
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     paddingVertical: 12,
-//     paddingHorizontal: 32,
-//     borderRadius: 4,
-//     backgroundColor: 'black',
-//   },
-//   text: {
-//     fontSize: 16,
-//     lineHeight: 21,
-//     fontWeight: 'bold',
-//     letterSpacing: 0.25,
-//     color: 'white',
-//   },
-// });
