bbbd11c676db991dcb0e5fae76adb4053d9a8d56
adrienneyl <adrienne.yl07@gmail.com>
2023-11-18 13:03:12 -0800

    Adding in Firebase auth stuff to avoid rebase

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = 3000;
 // Middleware to parse JSON data
 app.use(express.json());
+
+import { auth } from './firebase/auth.js';
 
 const mapsRouter = require('./routes/mapsRoutes');
 app.use('/maps', mapsRouter);
 
 const placesSearchRouter = require('./routes/placesSearchRoutes');
 app.use('/placesSearch', placesSearchRouter);
-
-
 
 
 // Start the Express server
