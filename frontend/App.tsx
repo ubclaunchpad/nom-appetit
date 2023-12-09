acf4bcb8a00e994b2962b52596dbb9d0cf71a071
Jason Lee <jason0409lee@gmail.com>
2023-12-09 12:24:55 -0800

    Co-authored-by: Roman Sinkus and Boris

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator, DrawerItem  } from '@react-navigation/drawer';
+import {useFonts} from 'expo-font';
+/*
+If you get any errors with the drawer import, try running the following commands in the frontend directory:
+
+$ npm install @react-navigation/drawer
+$ npm install react-native-gesture-handler react-native-reanimated
+*/
 
 import SplashScreen from "./src/screens/SplashScreen";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -12,27 +20,41 @@ import ViewRestaurantScreen from "./src/screens/ViewRestaurantScreen";
 import Result from "./src/screens/ResultScreen";
 import ForgotPassword from "./src/screens/ForgotPasswordScreen";
 import CreateAccountScreen from './src/screens/createAccountScreen';
+import SearchFriends from './src/screens/SearchFriendsScreen';
 
 const Stack = createNativeStackNavigator();
 
+// const RootNavigator = createNativeStackNavigator({
+//   Login: { screen: LoginScreen ,
+//            navigationBarStyle : {navBarHidden: true },
+//            navigationOptions: {
+//            headerShown: false,
+//            }
+//   },
+//   AfterLogin: { screen: MyDrawerNavigator,
+//     navigationBarStyle : {navBarHidden: true },
+//     navigationOptions: {
+//       headerShown: false,
+//     } },
+// });
+
 export default function App() {
+  const [loaded] = useFonts({
+  })
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Splash" component={SplashScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="Result" component={Result}/>
         <Stack.Screen name="Search" component={Search}/>
+        <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Selected" component={Selected}/>
         <Stack.Screen name="Signup" component={Signup}/>
         <Stack.Screen name="Suggest" component={Suggest}/>
         <Stack.Screen name="View" component={ViewRestaurantScreen}/>
         <Stack.Screen name="Forgot Password" component={ForgotPassword}/>
-        <Stack.Screen name="Create Account" component={CreateAccountScreen}/>
-
       </Stack.Navigator>
-    </NavigationContainer>
+  </NavigationContainer>
   );
 }
-
---
+++ frontend/src/screens/SearchFriendsScreen.tsx
@@ -0,0 +1,26 @@
+// home page
+import React, { useEffect, useState } from "react";
+import { Pressable, View, StyleSheet, Text, TextInput } from "react-native";
+
+
+export default function HomeScreen({navigation}) {
+    return (
+        <View style={style.container}>
+            <Text>
+                Search Friends Screen
+            </Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create({
+
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#DCE7C2',
+    }
+
+
+});
