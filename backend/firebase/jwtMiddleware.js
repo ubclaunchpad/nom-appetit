81a3efd627f16fb31b0a52a4066245736185a2d8
m20arcusk <marcuskam20@gmail.com>
2023-12-09 11:30:22 -0800

    added middleware function

---
+++ backend/firebase/jwtMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { firebaseAdmin } = require('./firebaseSetup'); // Import your Firebase Admin setup
+
+function jwtMiddleware(req, res, next) {
+  // Get the token from the request headers
+  const token = req.header('Authorization');
+
+  // Check if the token exists
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized: No token provided' });
+  }
+
+  // Verify the token
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => { // requires secret from .env file
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized: Invalid token' });
+    }
+
+    // Attach the decoded user information to the request object
+    req.user = decoded;
+
+    // Continue to the next middleware or route handler
+    next();
+  });
+}
+
+module.exports = jwtMiddleware;
