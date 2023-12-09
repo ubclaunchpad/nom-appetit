54a8abd762d07166a374e8c5b9ebf31d8745c69b
avivamei <58450418+avivamei@users.noreply.github.com>
2023-12-08 18:43:04 -0800

    Style buttons, Load Assets, and Fix Navigation Errors (#12)
    * fix type issues with navigation, add assets loader, style buttons
    
    * center button
    
    * fix type issue in RestaurantInfoComponent
    
    ---------
    
    Co-authored-by: avivamei <avivamei@students.cs.ubc.ca>

--- frontend/src/screens/SuggestRestaurantScreen.tsx
+++ frontend/src/screens/SuggestRestaurantScreen.tsx
@@ -1,13 +1,17 @@
 // suggest a restaurant page
 import React, { useEffect, useState } from "react";
 import { Button, View } from "react-native";
+import { RootStackParamList } from "src/types";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export default function SuggestRestaurantScreen({navigation}) {
+type Props = NativeStackScreenProps<RootStackParamList, "suggest">;
+
+export default function SuggestRestaurantScreen({navigation}: Props) {
     return (
         <View>
             <Button 
             title="Go Back"
-            onPress={ () => navigation.navigate('Home')}
+            onPress={ () => navigation.navigate('home')}
             />
         </View>
     )
