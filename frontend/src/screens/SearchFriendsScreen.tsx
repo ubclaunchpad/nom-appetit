fc798ff54b7b60340ad3b069ffea92dc4982c7ff
avivamei <avivamei@students.cs.ubc.ca>
2024-01-13 12:34:17 -0800

    merge conflicts

--- frontend/src/screens/SearchFriendsScreen.tsx
+++ frontend/src/screens/SearchFriendsScreen.tsx
@@ -1,26 +1,25 @@
-// home page
-import React, { useEffect, useState } from "react";
-import { Pressable, View, StyleSheet, Text, TextInput } from "react-native";
+// // home page
+// import React, { useEffect, useState } from "react";
+// import { Pressable, View, StyleSheet, Text, TextInput } from "react-native";
 
 
-export default function HomeScreen({navigation}) {
-    return (
-        <View style={style.container}>
-            <Text>
-                Search Friends Screen
-            </Text>
-        </View>
-    )
-}
+// export default function HomeScreen({navigation}) {
+//     return (
+//         <View style={style.container}>
+//             <Text>
+//                 Search Friends Screen
+//             </Text>
+//         </View>
+//     )
+// }
 
-const style = StyleSheet.create({
+// const style = StyleSheet.create({
 
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#DCE7C2',
-    }
+//     container: {
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//         backgroundColor: '#DCE7C2',
+//     }
 
-
-});
+// });
