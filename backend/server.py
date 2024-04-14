e00eef3eb25c9d696d42970c25bc487112617bd3
parkrafael <parkrafael11@gmail.com>
2024-04-13 17:54:54 -0700

    search implementation

--- backend/server.py
+++ backend/server.py
@@ -1,14 +1,25 @@
 # ===== imports =====
 from flask import Flask
 from flask import request
+from services.search import searchRestaurants
 
 # ===== flask configuration =====
 app = Flask(__name__)
 
 # ===== routing  =====
 @app.route("/search", methods=['GET', 'POST'])
+# example: /search?user_location=40.74229,-73.97771&keyword=pizza
 def search():
-    return "hello world"
+    try:
+        user_location = request.args.get('user_location')
+        keyword = request.args.get('keyword')
+        return searchRestaurants(user_location, keyword)
+       
+    except Exception as e:
+        raise Exception(f'An error occurred: {str(e)}')    
+    
+    
+        
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ backend/services/search.py
@@ -0,0 +1,34 @@
+# ===== imports =====
+from dotenv import load_dotenv, find_dotenv
+import requests
+import os
+
+# ===== api configuration =====
+load_dotenv('./services/config/.env')
+
+GOOGLE_KEY = os.getenv('GOOGLE_KEY')
+GOOGLE_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
+TYPE = 'restaurant'
+RADIUS = 2000
+
+if not GOOGLE_KEY:
+    raise Exception("API Key is either missing or invalid")
+
+# ===== functions =====
+def searchRestaurants(user_location, keyword):
+    params = {
+        'key': GOOGLE_KEY,
+        'type': TYPE,
+        'radius': RADIUS,
+        'location': user_location,
+        'query': keyword
+    }
+
+    try: 
+        response = requests.get(GOOGLE_URL, params=params)
+        return response.json()['results']
+    except Exception as e:
+        raise Exception(f'An error occurred: {str(e)}')     
+
+
+
