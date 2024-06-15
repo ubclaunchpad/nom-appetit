4dc9b499adda3d4bb337edc59cba0d12ce14f96e
Boris <74333521+bcw117@users.noreply.github.com>
2024-06-14 18:49:24 -0600

    Create create account/sign in screen with images (#29)
    * Complete create account and sign in screen with images
    
    * Add image imports
    
    * Change image naming
    
    * Add navigation to home screen in action buttons
    
    * Adjust screen width

--- frontend/src/images/Images.tsx
+++ frontend/src/images/Images.tsx
@@ -1,7 +1,10 @@
-const images = {
-    heartPNG: require("../images/heart.png"),
-    lighbulbPNG: require("../images/lightbulb.png"),
+const Images = {
+  heartPNG: require("../images/heart.png"),
+  lighbulbPNG: require("../images/lightbulb.png"),
+  pandaBamboo: require("../images/pandaBamboo.png"),
+  google_ic: require("../images/google_ic.png"),
+  facebook_ic: require("../images/facebook_ic.png"),
+  apple_ic: require("../images/apple_ic.png"),
 };
 
-
-export default images;
+export default Images;
