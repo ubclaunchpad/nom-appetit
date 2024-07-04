b67e019edb2d1854f704d09e605380557f19fef5
Boris <boris.wang117@gmail.com>
2024-07-04 15:40:29 -0600

    Initalize branch to edit searchScreens

--- frontend/src/screens/SearchScreen.tsx
+++ frontend/src/screens/SearchScreen.tsx
@@ -1,18 +1,138 @@
-// search restaurant
+// home page
 import React, { useEffect, useState } from "react";
-import { Button, View } from "react-native";
+import {
+  Button,
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  FlatList,
+  Pressable,
+  SafeAreaView,
+} from "react-native";
+import { RestaurantInfoComponent } from "../components/RestaurantInfoComponent";
 import { RootStackParamList } from "src/types";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { Icon } from "react-native-elements";
 
-type Props = NativeStackScreenProps<RootStackParamList, "searchRestaurant">;
+type Props = NativeStackScreenProps<RootStackParamList, "home">;
 
-export default function SearchRestaurantScreen({navigation}: Props) {
-    return (
-        <View>
-            <Button 
-            title="Execute Search"
-            onPress={ () => navigation.navigate('restaurantResult')}
+// boilerplate for future restaurants
+type Restaurant = {
+  type: string;
+  name: string;
+  rating: number;
+  distance: string;
+};
+const restaurants: Restaurant[] = [
+  {
+    type: "Chinese",
+    name: "Lin's Chinese Cuisine",
+    rating: 4,
+    distance: "1.2 km away",
+  },
+  {
+    type: "Italian",
+    name: "Trattoria",
+    rating: 3.5,
+    distance: "1.2 km away",
+  },
+  {
+    type: "Mexican",
+    name: "Taqueria",
+    rating: 2,
+    distance: "1.2 km away",
+  },
+  {
+    type: "Japanese",
+    name: "Koi Sushi",
+    rating: 5,
+    distance: "1.2 km away",
+  },
+];
+
+export default function SearchScreen({ navigation }: Props) {
+  return (
+    <SafeAreaView style={styles.containerBackground}>
+      <View style={styles.container}>
+        <Text
+          style={{
+            fontSize: 24,
+            textAlign: "center",
+            width: "75%",
+            marginVertical: 10,
+          }}
+        >
+          Search Restaurants
+        </Text>
+        <View style={styles.inputContainer}>
+          <Pressable>
+            <TextInput
+              onPress={() => navigation.navigate("searchRestaurant")}
+              style={styles.input}
+              placeholder="Search for a restaurant..."
+              placeholderTextColor={"#8FA68E"}
+              onSubmitEditing={() => navigation.navigate("selectedRestaurant")}
             />
+          </Pressable>
+          <View
+            style={{
+              padding: 10,
+              backgroundColor: "#f3cc91",
+              borderRadius: 12,
+            }}
+          >
+            <Icon
+              onPress={() => navigation.navigate("filterRestaurant")}
+              name="filter"
+              type="font-awesome"
+              color="#004643"
+              size={24}
+            />
+          </View>
         </View>
-    )
+
+        <FlatList
+          data={restaurants}
+          renderItem={({ item }) => <RestaurantInfoComponent {...item} />}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  containerBackground: {
+    width: "100%",
+    backgroundColor: "#dee7c5",
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  parentContainer: {
+    height: "100%",
+  },
+
+  inputContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 10,
+  },
+
+  input: {
+    backgroundColor: "#fffefa",
+    minHeight: 48,
+    color: "#9db19b",
+    borderColor: "#80a29e",
+    borderWidth: 1,
+    borderRadius: 12,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    width: 304,
+  },
+});
