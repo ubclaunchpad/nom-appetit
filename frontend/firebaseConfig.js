e95601de3e60d09a46e25a0e44b8d26b655150b9
Boris Wang <boris.wang117@gmail.com>
2024-08-29 13:58:57 -0600

    Add firebase config and template implementation of adding images to cloud storage

---
+++ frontend/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+};
+
+if (!firebase.app.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
