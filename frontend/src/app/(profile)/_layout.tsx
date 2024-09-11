214ecff78c8968d673dbc068c0f8bcdedf9971b5
Boris Wang <boris.wang117@gmail.com>
2024-09-11 11:04:03 -0700

    Remove friends, notifications and wishlist from front and backend

--- frontend/src/app/(profile)/_layout.tsx
+++ frontend/src/app/(profile)/_layout.tsx
@@ -8,12 +8,8 @@ const ProfileLayout = () => {
       }}
     >
       <Stack.Screen name="edit_profile" />
-      <Stack.Screen name="find_friends" />
-      <Stack.Screen name="friend_requests" />
-      <Stack.Screen name="friends_list" />
       <Stack.Screen name="profile" />
       <Stack.Screen name="saved_restaurants" />
-      <Stack.Screen name="notifications" />
     </Stack>
   );
 };
