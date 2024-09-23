b1889543fbd2447db46d02d94c06bfcd53740e82
Boris Wang <boris.wang117@gmail.com>
2024-09-22 20:42:06 -0700

    Add template for sign-in/register pages

--- frontend/src/app/sign-in.tsx
+++ frontend/src/app/sign-in.tsx
@@ -1,18 +1,40 @@
 import { router } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, View, TextInput } from "react-native";
 import { useSession } from "../context/SessionContext";
+import InputForm from "@components/InputForm";
+import { useState } from "react";
 
 export default function SignIn() {
   const { onLogin, authState } = useSession();
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userNotFoundMessage, setUserNotFoundMessage] = useState("");
+  const [invalidPasswordMessage, setInvalidPasswordMessage] = useState("");
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <InputForm
+        value={name}
+        onChangeText={setName}
+        placeholder="Username"
+        errorMessage={userNotFoundMessage}
+        autoCapitalize="none"
+        secureTextEntry={false}
+      />
+      <InputForm
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        errorMessage={invalidPasswordMessage}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
       <Text
         onPress={async () => {
-          await onLogin("admin", "password");
+          await onLogin(name, password);
           // Navigate after signing in. You may want to tweak this to ensure sign-in is
           // successful before navigating.
-          router.replace("/");
+          router.replace("/(tabs)");
         }}
       >
         Sign In
